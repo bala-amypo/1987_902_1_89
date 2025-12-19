95880979b4a977ddcc5dd88c58cd4ac3d051b046
bala-amypo <bala@amypo.in>
2025-12-19 06:22:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.example.backendproject.repository;
+import com.example.backendproject.model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+}
+
