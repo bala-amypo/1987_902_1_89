bdb6b1eec8d1f0feaad8f8535e273c5d32d708ad
bala-amypo <bala@amypo.in>
2025-12-26 09:59:09 +0000

    User commit

--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -4,10 +4,7 @@ import com.example.demo.model.Category;
 import java.util.List;
 
 public interface CategoryService {
-
     Category createCategory(Category category);
-
     Category getCategory(Long id);
-
     List<Category> getAllCategories();
 }
--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,22 +1,17 @@
 package com.example.demo.servlet;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.annotation.WebServlet;
-
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-
+    
     @Override
-    protected void doGet(
-            HttpServletRequest req,
-            HttpServletResponse resp
-    ) throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Hello from HelloServlet");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello from Servlet!</h1>");
     }
 }
-
